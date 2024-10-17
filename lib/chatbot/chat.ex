d5a1e8f55ebcd40af49d27fad9d152e8418b107a
Joel Koch <joel@bitcrowd.net>
2024-10-17 17:09:07 +0200

    Add form and assistant response

--- lib/chatbot/chat.ex
+++ lib/chatbot/chat.ex
@@ -1,8 +1,14 @@
 defmodule Chatbot.Chat do
   alias Chatbot.Chat.Message
 
-  def new_message(attrs) do
+  def new_user_message(%{role: :user} = attrs) do
     Message.changeset(attrs) |> Chatbot.Repo.insert!()
+  end
+
+  def new_assistant_message(messages) do
+    content = "blabla"
+
+    Message.changeset(%{role: :assistant, content: content}) |> Chatbot.Repo.insert!()
   end
 
   def all_messages() do
--- lib/chatbot_web/chat_live.ex
+++ lib/chatbot_web/chat_live.ex
@@ -1,10 +1,16 @@
 defmodule ChatbotWeb.ChatLive do
   use ChatbotWeb, :live_view
   alias Chatbot.Chat
+  import ChatbotWeb.CoreComponents
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :messages, Chat.all_messages())}
+    socket =
+      socket
+      |> assign(:messages, Chat.all_messages())
+      |> assign(:form, to_form(%{"message" => ""}))
+
+    {:ok, socket}
   end
 
   @impl Phoenix.LiveView
@@ -13,6 +19,27 @@ defmodule ChatbotWeb.ChatLive do
     <%= for m <- @messages do %>
       <p>role: <%= m.role %> content: <%= m.content %></p>
     <% end %>
+
+    <.simple_form for={@form} phx-submit="send">
+      <.input field={@form[:message]} label="Message" />
+      <:actions>
+        <.button>Send</.button>
+      </:actions>
+    </.simple_form>
     """
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("send", %{"message" => message}, socket) do
+    messages = socket.assigns.messages
+    user_message = Chat.new_user_message(%{role: :user, content: message})
+
+    messages = messages ++ [user_message]
+
+    _assistant_message = Chat.new_assistant_message(messages)
+
+    socket = assign(socket, :messages, Chat.all_messages())
+
+    {:noreply, socket}
   end
 end
